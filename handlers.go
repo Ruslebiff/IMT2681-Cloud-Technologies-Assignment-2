e6568a7c050894f99bc5de30c5f0bdd16e5a90c0
Ruslebiff <joakivb@stud.ntnu.no>
2019-11-01 22:38:03 +0100

    comments for handlers

--- handlers.go
+++ handlers.go
@@ -5,31 +5,37 @@ import (
 	"net/http"
 )
 
+// HandlerNil is the default http handler
 func HandlerNil(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Default Handler: Invalid request received.") // error to console
 	http.Error(w, "Invalid request", http.StatusBadRequest)   // error to http
 }
 
+// HandlerCommits returns the repos with highest numbers of commits, ?limit parameter
 func HandlerCommits(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Unimplemented handler used")                                        // error to console
 	http.Error(w, "This endpoint is not implemented yet", http.StatusNotImplemented) // error to http
 }
 
+// HandlerLanguages returns the languages used in given projects by dist. in descending order
 func HandlerLanguages(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Unimplemented handler used")                                        // error to console
 	http.Error(w, "This endpoint is not implemented yet", http.StatusNotImplemented) // error to http
 }
 
+// HandlerIssues returns the name of the users or labels (see parameters) for a given project
 func HandlerIssues(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Unimplemented handler used")                                        // error to console
 	http.Error(w, "This endpoint is not implemented yet", http.StatusNotImplemented) // error to http
 }
 
+// HandlerStatus returns information about availability of invoked service and database connectivity
 func HandlerStatus(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Unimplemented handler used")                                        // error to console
 	http.Error(w, "This endpoint is not implemented yet", http.StatusNotImplemented) // error to http
 }
 
+// HandlerWebhooks handles webhooks
 func HandlerWebhooks(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Unimplemented handler used")                                        // error to console
 	http.Error(w, "This endpoint is not implemented yet", http.StatusNotImplemented) // error to http
