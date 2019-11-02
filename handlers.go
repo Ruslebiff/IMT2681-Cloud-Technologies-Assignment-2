99ab48c0ceb74966d874c2d8e476f31d49af0881
Ruslebiff <joakivb@stud.ntnu.no>
2019-11-02 15:30:35 +0100

    HandlerCommits works

--- handlers.go
+++ handlers.go
@@ -3,7 +3,10 @@ package assignment2
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"sort"
+	"strconv"
 	"time"
 )
 
@@ -15,8 +18,99 @@ func HandlerNil(w http.ResponseWriter, r *http.Request) {
 
 // HandlerCommits returns the repos with highest numbers of commits, ?limit parameter
 func HandlerCommits(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Unimplemented handler used")                                        // error to console
-	http.Error(w, "This endpoint is not implemented yet", http.StatusNotImplemented) // error to http
+	var c = &Commit{}
+
+	var repos []Repo
+	var results []Result
+
+	limit := r.URL.Query().Get("limit")
+	auth := r.URL.Query().Get("auth")
+
+	if limit == "" { // if no limit in url
+		limit = "5" // set default limit
+	}
+
+	if auth != "" {
+		c.Auth = true
+	} else {
+		c.Auth = false
+	}
+
+	url1 := GitLabRoot + "v4/projects/"
+	url2 := GitLabRoot + "v4/projects?per_page=1"
+
+	resp, err := http.Get(url2 + "&private_token=" + auth)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+
+	totalpages, err := strconv.Atoi(resp.Header.Get("X-Total-Pages"))
+	if err != nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	}
+
+	//	totalRepositories, err := strconv.Atoi(resp.Header.Get("X-Total"))
+	//	if err != nil {
+	//		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	//	}
+
+	for i := 1; i <= totalpages; i++ {
+		var temp []Repo
+
+		pagenumber := strconv.Itoa(i)
+		resp, err := http.Get(url2 + "&page=" + pagenumber + "&private_token=" + auth)
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		jsonresponse, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Println(http.StatusInternalServerError)
+			return
+		}
+
+		json.Unmarshal([]byte(jsonresponse), &temp)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		repos = append(repos, temp...) // add each page to repos array
+	}
+
+	for _, j := range repos {
+		resp, err := http.Get(url1 + strconv.Itoa(j.ID) + "/repository/commits?private_token=" + auth)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Println(http.StatusInternalServerError)
+			return
+		}
+
+		totalcommits, err := strconv.Atoi(resp.Header.Get("X-total"))
+		if err != nil {
+			totalcommits = 0
+		}
+
+		resultsappend := Result{Repository: j.Reponame, Commits: totalcommits}
+		results = append(results, resultsappend)
+	}
+
+	sort.Slice(results, func(i, k int) bool { // Sorting by # of commits
+		return results[i].Commits > results[k].Commits
+	})
+
+	limitint, err := strconv.Atoi(limit)
+	if err != nil {
+		return
+	}
+
+	for i := 1; i <= limitint; i++ {
+		c.Repos = append(c.Repos, results[i])
+	}
+
+	http.Header.Add(w.Header(), "content-type", "application/json") // json header
+	json.NewEncoder(w).Encode(c)                                    // Encode struct to JSON
 }
 
 // HandlerLanguages returns the languages used in given projects by dist. in descending order
--- structs.go
+++ structs.go
@@ -7,3 +7,21 @@ type Status struct {
 	Uptime   float64
 	Version  string
 }
+
+// Result struct
+type Result struct {
+	Repository string
+	Commits    int
+}
+
+// Repo struct for json
+type Repo struct {
+	ID       int    `json:"id"`
+	Reponame string `json:"path_with_namespace"`
+}
+
+// Commit struct
+type Commit struct {
+	Repos []Result
+	Auth  bool
+}
