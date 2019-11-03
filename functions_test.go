de06400c2523e9659f7a0f351fcc934d2939254f
Ruslebiff <joakivb@stud.ntnu.no>
2019-11-03 23:25:56 +0100

    test_dbsave

--- functions_test.go
+++ functions_test.go
@@ -1,9 +1,24 @@
 package assignment2
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func Test_DBInit(t *testing.T) {
 	err := DBInit()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_DBSave(t *testing.T) {
+	webhook := Webhookreg{}
+
+	webhook.Event = "commits"
+	webhook.Time = time.Now()
+
+	_, err := DBSave(&webhook)
 	if err != nil {
 		t.Error(err)
 	}
