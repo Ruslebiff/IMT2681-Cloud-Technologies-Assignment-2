214faa01338206042f39a486ad433750ab56d4bf
Ruslebiff <joakivb@stud.ntnu.no>
2019-11-03 23:44:34 +0100

    added more tests

--- functions_test.go
+++ functions_test.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+var Testid = ""
+
 func Test_DBInit(t *testing.T) {
 	err := DBInit()
 	if err != nil {
@@ -18,8 +20,48 @@ func Test_DBSave(t *testing.T) {
 	webhook.Event = "commits"
 	webhook.Time = time.Now()
 
-	_, err := DBSave(&webhook)
+	var err error
+	Testid, err = DBSave(&webhook)
 	if err != nil {
 		t.Error(err)
 	}
 }
+
+func Test_DBReadid(t *testing.T) {
+	webhook, err := DBReadid(Testid)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if webhook.ID != Testid {
+		t.Error(err)
+	}
+}
+
+func Test_DBDelete(t *testing.T) {
+	err := DBDelete(Testid)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_DBReadall(t *testing.T) {
+	var webhooks []Webhookreg    // temp storage for all webhooks from db
+	webhooks, err := DBReadall() // read from db into webhooks array
+	if err != nil {
+		t.Error(err)
+	}
+
+	if webhooks == nil {
+		t.Error(err)
+	}
+}
+
+/*
+func Test_DBClose(t *testing.T) {
+	err := DBClose()
+	if err != nil {
+		t.Error(err)
+	}
+}
+*/
